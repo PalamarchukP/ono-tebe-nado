a24298a4ec5b27b74f9f718af2c1206cf114cd15
Polina Palamarchuk <polina.palamarchuk14@gmail.com>
2024-10-31 16:39:47 +0200

    первый коммит

---
+++ main.js
@@ -0,0 +1,16 @@
+// function deadLine(){
+
+// }
+
+// document.querySelector('click', deadLine(){
+//     a.className = 'header-list-item-active'
+//    a.style.underline = none
+// })
+
+
+
+// const li1 = document.querySelector("#li1");
+
+// li1.addEventListener('click', ()=>{
+//     li1.className = "WithOutLine";
+// })
